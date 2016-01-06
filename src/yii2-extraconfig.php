c43f9e21238bc20aaaf402013202cb7f01acaaeb
Andrii Vasyliev <sol@hiqdev.com>
2016-01-06 12:40:40 +0000

    redoing with yii2-extraconfig

--- src/yii2-extraconfig.php
+++ src/yii2-extraconfig.php
@@ -9,29 +9,24 @@
  * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
  */
 
-namespace hidev\phpunit;
-
-class Plugin extends \hiqdev\pluginmanager\Plugin
-{
-    protected $_items = [
+return [
+    'components' =>  [
         'goals' => [
             'phpunit'          => 'hidev\phpunit\goals\PhpunitGoal',
             'phpunit.xml.dist' => 'hidev\phpunit\goals\PhpunitXmlGoal',
         ],
-        'components' =>  [
-            'binaries' => [
-                'phpunit' => [
-                    'package' => 'phpunit/phpunit',
-                    'version' => '^4.8',
-                ],
-                'phpunit-skelgen' => [
-                    'package' => 'phpunit/phpunit-skeleton-generator',
-                    'version' => '*',
-                ],
+        'binaries' => [
+            'phpunit' => [
+                'package' => 'phpunit/phpunit',
+                'version' => '^4.8',
+            ],
+            'phpunit-skelgen' => [
+                'package' => 'phpunit/phpunit-skeleton-generator',
+                'version' => '*',
             ],
         ],
         'views' => [
             '@hidev/phpunit/views',
         ],
-    ];
-}
+    ],
+];
